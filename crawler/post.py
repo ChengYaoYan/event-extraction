d6fc83b84c084a482719c21f0e62022f5630530c
ChengYaoYan <1526695730@qq.com>
2021-03-18 08:21:21 +0800

    post

--- crawler/post.py
+++ crawler/post.py
@@ -1,8 +1,3 @@
-'''
-抓取百度贴吧---生活大爆炸吧的基本内容
-爬虫线路： requests - bs4
-Python版本： 3.7
-'''
 import requests
 import time
 from bs4 import BeautifulSoup
@@ -17,11 +12,50 @@ def get_html(url):
 	except:
 		return 'Something wrong happend!'
 
+
 def get_content(url):
 	posts = []
 	soup = BeautifulSoup(get_html(url), 'lxml')
+	li_tags = soup.find_all('li', attrs={'class': 'j_thread_list clearfix thread_item_box'})
 
-	li_tags = soup.findall('li', attrs={'class': ' j_thread_list clearfix thread_item_box'})
-	print(li_tags[0])
+	for li in li_tags:
+		post = dict()
 
-get_content('https://tieba.baidu.com/f?kw=%E7%94%9F%E6%B4%BB%E5%A4%A7%E7%88%86%E7%82%B8&ie=utf-8&pn=100')
+		try: 
+			post['title'] = li.find('a', attrs={'class': 'j_th_tit'}).text.strip()
+			post['link'] = 'http://tieba.baidu.com/' + li.find('a', attrs={'class': 'j_th_tit'})['href']
+			post['name'] = li.find('a', attrs={'class': 'frs-author-name'}).text
+			post['time'] = li.find('span', attrs={'title': '创建时间'}).text
+			post['replayNum'] = li.find('span', attrs={'title': '回复'}).text
+			posts.append(post)
+		except: print('Something wrong happend!')
+
+	return posts
+
+
+def write_to_file(posts):
+	with open('TTBT.txt', 'a+') as file_object:
+		for post in posts:
+			file_object.write(f'title: {post["title"]} \t link: {post["link"]} \t name: {post["name"]} \t time: {post["time"]} \t replayNum: {post["replayNum"]}\n')
+
+		print('Saved finish.')
+
+
+def main(base_url, page_number):
+	urls_list = []
+
+	for pn in range(0, page_number):
+		urls_list.append(base_url + str(50 * pn))
+	print('All page have been downloaded already, readying to extract information.')
+
+	for url in urls_list:
+		content = get_content(url)
+		write_to_file(content)
+	print('Saved all information.')
+
+base_url = 'https://tieba.baidu.com/f?kw=%E7%94%9F%E6%B4%BB%E5%A4%A7%E7%88%86%E7%82%B8&ie=utf-8&pn='
+page_number = 3
+
+if __name__ == '__main__':
+	main(base_url, page_number)
+
